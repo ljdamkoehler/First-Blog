2d09b9b65508fe740b1bc1b64358336f8038e075
Luke Damkoehler <ljdamkoehler@gmail.com>
2022-06-13 20:05:53 -0500

    Impliment PostCard with all basic styles and fetching from graphcms api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  {title: 'Learning Python', excerpt: 'My journey learning how to code in Python'},
-  {title: 'Learning JavaScript', excerpt: 'My journey learning how to code in JavaScript'}
-];
+// Demo posts
+// const posts = [
+//   {title: 'Learning Python', excerpt: 'My journey learning how to code in Python'},
+//   {title: 'Learning JavaScript', excerpt: 'My journey learning how to code in JavaScript'}
+// ];
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -15,7 +17,7 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post, index) => <PostCard post={post.node} key={post.title} />)}
         </div>
         
         <div className='lg: col-span-4 col-span-1'>
@@ -30,6 +32,14 @@ export default function Home() {
   
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
 
 
